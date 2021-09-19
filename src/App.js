8c3a6e38df8ce4cd1acc793ef6dd079e3f506f0b
shubhamku044 <shubhamku044@gmail.com>
2021-09-19 22:53:17 +0530

    complete website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from './components/header/header.component';
 import HomePage from './pages/home/home.page';
 import AboutPage from './pages/about/about.page';
+import ProjectsPage from './pages/project/projects.page';
 import Footer from './components/footer/footer.component';
+import TestimonialPage from './pages/testimonial/testimonials.page';
 import SocialHandle from './components/socials/social-handles.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
@@ -14,6 +16,8 @@ function App() {
 			<Switch>
 				<Route exact path="/portfolio" component={HomePage} />
 				<Route exact path="/about" component={AboutPage} />
+				<Route exact path="/projects" component={ProjectsPage} />
+				<Route exact path="/testimonials" component={TestimonialPage} />
 			</Switch>
 			<Footer />
 		</div>
